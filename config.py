4d755b2ed124b7cbe05cf82fa56c90392eec917b
xhpmoonx <mahshad.a.sharifi@gmail.com>
2025-04-09 21:49:22 -0700

    Assign orders to nearest available robot using shortest A* path

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ sample_orders = [
     {'order_id': 3, 'location': (15, 10), 'assigned': False}
 ]
 
-MAP_SIZE = 30 # The width and height of the square grid 
+MAP_SIZE = 25 # The width and height of the square grid 
 OBSTACLE_RATIO = 0.22 # The fraction of the grid to fill with obstacles
 FW_LOCATION = (0, 0)
 SP_LOCATION  = [order['location'] for order in sample_orders]
--- main.py
+++ main.py
@@ -6,14 +6,36 @@ from config import sample_orders
 
 
 if __name__ == '__main__':
-    campus_map, _ = generate_map()
-    for robot, order in zip(sample_robots, sample_orders):
+    campus_map, _ = generate_map() # Returns a representation of the environmen
+    available_robots = sample_robots[:]  # make a copy so we can remove assigned ones
+
+    for order in sample_orders:
+        closest_robot = None
+        shortest_path = None
+
+        for robot in available_robots:
+            path = astar(campus_map, robot.position, order['location'])
+            if path and (shortest_path is None or len(path) < len(shortest_path)):
+                closest_robot = robot
+                shortest_path = path
+
+        if closest_robot and shortest_path:
+            order['assigned'] = True
+            closest_robot.set_path(shortest_path, order)
+            available_robots.remove(closest_robot)
+        else:
+            print(f"[ERROR] No valid robot for Order {order['order_id']} at {order['location']}")
+    
+    ''' # Prevoius Version Used to Zip the orders to robots in parallel
+        for robot, order in zip(sample_robots, sample_orders):
         path = astar(campus_map, robot.position, order['location'])
         if path:
             order['assigned'] = True
             robot.set_path(path, order)
         else:
             print(f"[ERROR] No valid path for Robot {robot.robot_id} to Order {order['order_id']} at {order['location']}")
+
+    '''
 
     sim = Simulation(campus_map, sample_orders, sample_robots)
     sim.run()
