c80f3ec64ee7715e3ffe569cb5a1d352d23a9954
xhpmoonx <mahshad.a.sharifi@gmail.com>
2025-04-18 22:49:32 -0700

    multiple orders to be available on the first batch

--- main.py
+++ main.py
@@ -45,4 +45,12 @@ if __name__ == '__main__':
     """
     # Initialize and start the simulation with the map, orders, and robots
     sim = Simulation(campus_map, [], sample_robots)
+    # Pre-fill with 12 orders
+    for _ in range(12):
+        new_order = sim.generate_order()
+        new_order['order_id'] = sim.order_id_counter
+        sim.orders.append(new_order)
+        sim.order_id_counter += 1
+
+    sim.assign_orders_in_batch()
     sim.run() # Launches the visual simulation using Pygame
